af47daba8c998369073288b621a5d16e88cb199d
che <466934322@qq.com>
2021-09-15 16:21:38 +0800

    no message

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM cikiche001/nginx_php 
+LABEL maintainer="466934322@qq.com"
+
+COPY code.sh /code.sh
+
+RUN set -x; chmod +x /code.sh 
+
+ENTRYPOINT [ "sh", "/code.sh" ]
---
+++ code.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+cd /var/www/html
+
+git clone https://github.com/iqiqiya/iqiqiya-API.git iqiqiya
+
+
+chown -R www-data:www-data iqiqiya
+chmod -R 0755 iqiqiya
